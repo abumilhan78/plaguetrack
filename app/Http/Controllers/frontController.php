4a0d242e82dd0577fc553615ec74eb64319e512a
abumilhan78 <abumilhan78@gmail.com>
2021-02-24 08:36:32 +0700

    initial release

--- app/Http/Controllers/frontController.php
+++ app/Http/Controllers/frontController.php
@@ -23,7 +23,7 @@ class frontController extends Controller
     			->join('districts', 'districts.id', '=', 'subdistricts.dist_id')
     			->join('cities', 'cities.id', '=', 'districts.city_id')
     			->join('provinces', 'provinces.id', '=', 'cities.prov_id')
-    			->groupBy('provinces.prov_name')
+    			->groupBy('provinces.id')
     			->get();
         $datas =  Http::get('https://api.kawalcorona.com/')->json();
         $dt_global = [];
